84fac5f26dfeb2367548c14bec15dcb2e7fcbd78
joaopariz <parizoficial0102@outlook.com>
2023-03-26 22:28:42 -0300

    feat: create archive for controller

--- app/Http/Controllers/ClientController.php
+++ app/Http/Controllers/ClientController.php
@@ -3,62 +3,32 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\Http\Controllers\Controller;
+use App\Models\Client;
 
 class ClientController extends Controller
 {
-    /**
-     * Display a listing of the resource.
-     */
-    public function index()
+
+    public function index() // Route primary
     {
-        //
+        return Client::all();
     }
 
-    /**
-     * Show the form for creating a new resource.
-     */
-    public function create()
+
+
+    public function store(Request $request) // Send to data base
     {
-        //
+        $request->validate([
+            'client' => 'required',
+            'address' => 'required',
+            'city' => 'required',
+            'zip_code' => 'required',
+            'phone' => 'required',
+        ]);
     }
 
-    /**
-     * Store a newly created resource in storage.
-     */
-    public function store(Request $request)
+    public function show($id) // Bring data base info
     {
-        //
-    }
-
-    /**
-     * Display the specified resource.
-     */
-    public function show(string $id)
-    {
-        //
-    }
-
-    /**
-     * Show the form for editing the specified resource.
-     */
-    public function edit(string $id)
-    {
-        //
-    }
-
-    /**
-     * Update the specified resource in storage.
-     */
-    public function update(Request $request, string $id)
-    {
-        //
-    }
-
-    /**
-     * Remove the specified resource from storage.
-     */
-    public function destroy(string $id)
-    {
-        //
+        return Client::findOrfail($id);
     }
 }
